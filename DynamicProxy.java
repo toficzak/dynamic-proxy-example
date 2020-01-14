337be9afcd151d7d1e2baf18e6e3bb537a54916c
Krzysztof Kucharczyk <krzysztof.kucharczyk@hotmail.com>
2020-01-14 21:38:31 +0100

    Dynamic proxy example.

---
+++ DynamicProxy.java
@@ -0,0 +1,66 @@
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.util.HashMap;
+import java.util.Map;
+
+interface Human {
+    void walk();
+
+    void talk();
+}
+
+class Person implements Human {
+
+    @Override
+    public void walk() {
+        System.out.println("I am walking");
+    }
+
+    @Override
+    public void talk() {
+        System.out.println("I am talking");
+    }
+
+}
+
+class LoggingHandler implements InvocationHandler {
+
+    private final Object target;
+    private Map<String, Integer> calls = new HashMap<>();
+
+    public LoggingHandler(Object target) {
+        this.target = target;
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        String name = method.getName();
+
+        if (name.contains("toString")) {
+            return calls.toString();
+        }
+
+        calls.merge(name, 1, Integer::sum);
+        return method.invoke(target, args);
+    }
+
+}
+
+class DynamicProxy {
+    @SuppressWarnings("Unchecked")
+    public static <T> T withlogging(T target, Class<T> itf) {
+        return (T) Proxy.newProxyInstance(itf.getClassLoader(), new Class<?>[] { itf }, new LoggingHandler(target));
+    }
+
+    public static void main(String[] args) {
+        Person person = new Person();
+        Human logged = withlogging(person, Human.class);
+
+        logged.talk();
+        logged.walk();
+        logged.walk();
+
+        System.out.println(logged);
+    }
+}
